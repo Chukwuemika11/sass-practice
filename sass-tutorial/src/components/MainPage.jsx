1c7341169603420196946ac2eb3004d7b51f9ca7
unknown <chukwuemika11>
2024-03-29 22:49:28 +0100

    sass practice

---
+++ sass-tutorial/src/components/MainPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../../../dist/css/MainPage/MainPage.css'
+
+const MainPage = () => {
+  return (
+    <div className='mainPage'>
+      <h1>Welcome to our website</h1>
+     <button>click here</button>
+    </div>
+  )
+}
+
+export default MainPage
